ba49f51ba3cfec57ceca06b09ad60a2b225d245f
MichaelObvious <michele.palese42@outlook.it>
2024-01-17 15:37:52 +0100

    sentences

--- sententiae.js
+++ sententiae.js
@@ -40,7 +40,7 @@ let sentences = [
         from: 'Hippocratis Aphorismon',
     },
     {
-        text: 'Tempus rerum edax',
+        text: 'Tempus edax rerum, tuque, invidiosa vetustas, omnia destruitis vitiataque dentibus aevi paulatim lenta consumitis omnia morte!',
         from: 'Ovidii Metamorphoseon XV',
     },
     {
@@ -70,6 +70,158 @@ let sentences = [
     {
         text: 'Est omnino nulla opus tempus persequi; non aufugit a nobis, sed ad nos accurrit',
         from: 'Antonii Bloom Incipio orationis',
+    },
+    {
+        text: 'Dinumerare dies nostros sic doce nos, ut inducamus cor ad sapientiam',
+        from: 'Psalmo XC (LXXXIX)',
+    },
+    {
+        text: 'Horas numero, nec modo serenas',
+        from: 'ludo verborum',
+    },
+    {
+        text: 'Sensim sed propere fluit irremeabilis hora: consule ne perdas absque labore diem',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Tempus scire cupis? Dicam propere: ut homo honestus tempus est agere',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Quod addo, detraho vitae',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Quod morire patet, qua morire latet',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Esse, fuisse, fore, tria florida sunt sine flore, / omne simul periit quod fuit, est, et erit. / Quod fuit, est, et erit, periit spatio brevis horae, / ergo parum refert esse, fuisse, fore.',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Quot horarum lapsus, tot ad mortem passus',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Quid stas? Transit hora',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Quota est hora? Forsitan tua',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Memento nos ambo dinumerare: tu meas, ego tuas.',
+        from: '',
+    },
+    {
+        text: 'Senescis aspiciendo',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Die dies traditur',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Dum tempus habemus, operemur bonum',
+        from: 'ad Galatas Epistula VI',
+    },
+    {
+        text: 'Errare potest faber, errare potest ferro, nec ego autem erro',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Finiet una labores',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Haec ultima multis',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Hora est iam nos de somno surgere',
+        from: 'ad Romanos Epistula XIII',
+    },
+    {
+        text: 'Discat homo dinumerare horas, quot illarum vivit, tot breviat moras',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Iam propera nec te venturas differ in horas, qui non est hodie, cras minus aptus erit',
+        from: 'Ovidii Remediis Amoris',
+    },
+    {
+        text: 'Labora dum lucet',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Dies dividitur in horas duodecim bis, in una postremam non recensebis',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Mane piger stertis, fugit hora',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Maneo nemini',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Me ortum vides, forsan non occasum',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Meam non tuam novisti',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Noli abutere',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Ne differas de die in diem',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Nulla dies sine linea',
+        from: 'Plinii Naturali historia XXXV',
+    },
+    {
+        text: 'Nulli optabilis dabitur mora, irrevocabilis labitur hora: ne sit inutilis, semper labora, neve sis futilis, vigila, ora:',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Numquam reditura',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Horam aspice, iamque abitam, solis metior passus, hominis vitam',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Omnibus exoritur',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Omnibus brevis, ultima multis',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Silens loquor',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Sol lucet omnibus',
+        from: 'Meridiana quadam',
+    },
+    {
+        text: 'Sol non occidat super iracundiam vestram',
+        from: 'ad Ephesios epistula IV',
+    },
+    {
+        text: 'Soli soli soli',
+        from: 'verborum ludo',
     },
     {
         text: ' ',
