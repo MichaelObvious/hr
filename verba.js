6c8f98bdec16e91655ecdc0baf5dac2721d291f1
MichaelObvious <michele.palese42@outlook.it>
2024-01-19 12:48:06 +0100

    verba

--- verba.js
+++ verba.js
@@ -242,5 +242,72 @@ let verba = [
     {
         text: 'Si vult homo, in una die usque ad vesperam pervenit ad mensuram divinitatis',
         from: 'Apophtegmatis Patrum, Abbae Alonii verbis'
+    },
+    {
+        text: 'O dives, dives, non omni tempore vives! Fac bene dum vivis, post mortem vivere si vis!',
+        from: 'Philosophia Patrum',
+    },
+    {
+        text: 'Quanto maior eris, maiora pericla cavenda; Crede mihi: nullo tempore tutus eris',
+        from: 'Michaelis Verini Distichis',
+    },
+    {
+        text: 'Temporis in mundo res nulla fugacior hora; Quam cum praesentem credimus, illa fuit',
+        from: 'François Oudin Silva Distichorum',
+    },
+    {
+        text: 'Omnia tempus habent: flebis dum tempora flendi, ride dum risum tempora laeta ferunt',
+        from: 'Ioseph Gatti Salibus Poeticis, Proverbialibus, et Iocosis'
+    },
+    {
+        text: 'Rebus in humanis adeo in se cuncta recurrunt, finis ut unius sit caput alterius',
+        from: 'Joachim Camerarii Symbolis'
+    }, {
+        text: 'Nil magis est nostrum volucris quam temporis usus, sed multis tamquam res aliena volat.',
+        from: 'Michaelis Verini Distichis'
+    },
+    {
+        text: 'Res est una, tuam possis quam dicere: tempus. Utere! Dum cessas, desinit esse tuum.',
+        from: 'François Oudin Silva Distichorum'
+    },
+    {
+        text: 'Cuncta trahit secum vertitque volubile tempus, nec patitur certa currere quemque via',
+        from: 'Adagiorum Maxime Vulgarium Thesauro'
+    },
+    {
+        text: 'Disce, puer, dum tempus habes, ne tempora perdas; Tempora praetereunt more fluentis aquae',
+        from: 'Florilegio Gottingensi',
+    },
+    {
+        text: 'Disce velut nullo umquam sis moriturus in aevo; Vitam, ceu semper sis moriturus, age',
+        from: 'Urbani Appendini De Educatione Disticha'
+    },
+    {
+        text: 'Quam bene, non quanto vivamus tempore refert; sat vixit, vitam qui sine labe tulit',
+        from: 'Iohannis Pignevvart Catone Bernardino'
+    },
+    {
+        text: 'Aspice quam celeri cursu levis effugit hora, lubrica nec vitae tempora perde tuae',
+        from: 'Ioseph Gatti Salibus Poeticis, Proverbialibus, et Iocosis'
+    },
+    {
+        text: 'Quid rem scire iuvat, nisi scis quo tempore agenda prudenter, quo sit res quoque tuta loco?',
+        from: 'Iacobi Bornitz Symbolis et Emblematis'
+    },
+    {
+        text: 'Si mora nulla datur vitae labentibus horis, cur ita tam longo turba sopore iacet?',
+        from: 'Michaelis Verini Distichis'
+    },
+    {
+        text: 'Si fueris sapiens, Croesi superaveris aurum, nam sapiens nullo tempore vivit inops',
+        from: 'Michaelis Verini Distichis',
+    },
+    {
+        text: 'Disce, quod ignoras, ne perdas tempus et horas; res praedifficilis facilis fit cuique volenti',
+        from: 'Florilegio Gottingensi'
+    },
+    {
+        text: 'Vivamus puri quasi simus cras morituri;Discamus veluti simus de tempore tuti',
+        from: 'Philosophia Patrum'
     }
 ];
