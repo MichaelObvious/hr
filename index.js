1a1473c90d3135473a3aa879f7177b12e2a7dd31
MichaelObvious <michele.palese42@outlook.it>
2024-01-25 11:47:01 +0100

    state saving

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var ANALOG = false;
 
 window.onload = () => {
+    ANALOG = "true" === localStorage.getItem("analog");
+
     let toggleState = () => {
         ANALOG = !ANALOG;
+        localStorage.setItem("analog", ANALOG);
 		updateHorologium();
         updateInscription();
     }
+
     let clock_div = document.getElementsByClassName("clockd")[0];
     clock_div.setAttribute('href', "#");
 	clock_div.onclick = toggleState;
+    
     document.addEventListener('keyup', (e) => {
         if (e.code === "Space") toggleState();
     });
