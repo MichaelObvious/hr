b4c1e76b55e87390e21418b756a7cef9c194ad04
MichaelObvious <michele.palese42@outlook.it>
2024-01-25 11:40:40 +0100

    space

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var ANALOG = false;
 
 window.onload = () => {
-    let clock_div = document.getElementsByClassName("clockd")[0];
-    clock_div.setAttribute('href', "#");
-	clock_div.onclick = () => {
-		ANALOG = !ANALOG;
+    let toggleState = () => {
+        ANALOG = !ANALOG;
 		updateHorologium();
         updateInscription();
-	}
+    }
+    let clock_div = document.getElementsByClassName("clockd")[0];
+    clock_div.setAttribute('href', "#");
+	clock_div.onclick = toggleState;
+    document.addEventListener('keyup', (e) => {
+        if (e.code === "Space") toggleState();
+    });
 
     loadHorologium();
     loadInscription();
